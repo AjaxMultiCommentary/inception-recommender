d20ce2cda4e773640d09ab714e6747c836b1fde5
Matteo Romanello <matteo.romanello@epfl.ch>
2023-11-02 12:08:28 +0100

    added working recommender

---
+++ inception_recommender.py
@@ -0,0 +1,73 @@
+import logging
+from flair.data import Sentence, Token
+from flair.models import SequenceTagger
+from cassis import load_cas_from_xmi, load_typesystem, Cas, TypeSystem, merge_typesystems
+from ariadne.server import Server
+from ariadne.classifier import Classifier
+from ariadne.util import setup_logging
+from ariadne.contrib.inception_util import create_prediction, TOKEN_TYPE, IS_PREDICTION
+from ariadne.contrib.inception_util import SENTENCE_TYPE
+
+PREDICTED_TYPE = "webanno.custom.AjMCNamedEntity"
+PREDICTED_FEATURE = "value"
+USER = "mromanello"
+PROJECT_ID = "test_project"
+
+setup_logging(level=logging.DEBUG)
+
+
+class ClassicsNERClassifier(Classifier):
+    
+    def __init__(self, language : str, tagset : str):
+        self._model = SequenceTagger.load("hmteams/flair-hipe-2022-ajmc-fr")
+
+    def predict(self, cas: Cas, layer: str, feature: str, project_id: str, document_id: str, user_id: str):
+        tokens_cas = list(cas.select(TOKEN_TYPE))
+        sentences = cas.select(SENTENCE_TYPE)
+        text = []
+        idx = 0
+        for sentence in sentences:
+            tokens = [
+                Token(t.get_covered_text())
+                for t in cas.select_covered(TOKEN_TYPE, sentence)
+            ]
+            for token in tokens:
+                token.index = idx
+                idx += 1
+            s = Sentence(tokens)
+            text.append(s)
+        
+        self._model.predict(text)
+
+        for sentence in text:
+            for ent in sentence.get_spans('ner'):
+                start_idx = ent.tokens[0].index
+                end_idx = start_idx + len(ent.tokens) - 1
+                begin = tokens_cas[start_idx].begin
+                end = tokens_cas[end_idx].end
+                prediction = create_prediction(cas, layer, feature, begin, end, ent.tag)
+                cas.add(prediction)
+
+def build_typesystem() -> TypeSystem:
+    typesystem = TypeSystem()
+    SentenceType = typesystem.create_type(SENTENCE_TYPE)
+    PredictedType = typesystem.create_type(PREDICTED_TYPE)
+    typesystem.create_feature(PredictedType, PREDICTED_FEATURE, "uima.cas.String")
+    typesystem.create_feature(PredictedType, IS_PREDICTION, "uima.cas.Boolean")
+    return typesystem
+
+def load_test_document() -> Cas:
+    with open("data/AjMC_TypeSystem.xml", "rb") as f:
+        typesystem = merge_typesystems(load_typesystem(f), build_typesystem())
+
+    with open("data/lestragdiesdeso00tourgoog_0065.xmi", "rb") as f:
+        cas = load_cas_from_xmi(f, typesystem=typesystem)
+
+    return cas
+
+server = Server()
+server.add_classifier("ajmc_ner_fr", ClassicsNERClassifier("fr", "coarse"))
+app = server._app
+
+if __name__ == '__main__':
+    server.start(debug=True)
